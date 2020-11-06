7f3d3a6316eea28786a168fafd09d8e2821ad889
taiwocoker <taiwofcoker@gmail.com>
2020-11-06 12:15:21 +0100

    Add user model validation

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates_uniqueness_of :username
+  validates :username, :email, presence: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+end
+
+
