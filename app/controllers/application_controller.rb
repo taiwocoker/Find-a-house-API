021dc5c4299106a455b8831654399afac606a517
taiwocoker <taiwofcoker@gmail.com>
2020-11-13 16:59:19 +0100

    Add handling error to unique constraints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::Serialization
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActiveRecord::RecordInvalid, with: :invalid
-  
+  rescue_from ActiveRecord::RecordNotUnique, with: :not_unique
   private
   def not_found(errors)
     render json: errors, status: :not_found
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::API
   def invalid(errors)
     render json: errors, status: :unprocessable_entity
   end
+  def not_unique(errors)
+    render json: errors, status: :internal_server_error
+  end
 end
+
