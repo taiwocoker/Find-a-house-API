3ca3ae4a474c5a20d9533b9c325f5efa869a6ae2
taiwocoker <taiwofcoker@gmail.com>
2020-11-13 16:15:40 +0100

    Add route to the favourite

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       resources :houses, only: [:index, :show, :create]
+      post 'favourites', to: 'users#new_favourite'
     end
   end
 end
