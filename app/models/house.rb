e06479c553ebaa81b1a15c105720a71c17c9f7f6
taiwocoker <taiwofcoker@gmail.com>
2020-11-13 09:14:45 +0100

    Build house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,11 @@
 class House < ApplicationRecord
+  has_many :favourites
+  has_many :users, through: :favourites
+  has_one_attached :image
+  validates :name, :description, :image, presence: true
+
+  def img_url
+    Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true)
+  end
+
 end
