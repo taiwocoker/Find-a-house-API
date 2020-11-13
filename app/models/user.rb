78ba29c9ddab5d6167ee5737ff9fefa71ecdf50b
taiwocoker <taiwofcoker@gmail.com>
2020-11-13 09:20:23 +0100

    update user model with favourite association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :favourites
+  has_many :houses, through: :favourites
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates_uniqueness_of :username
   validates :username, :email, presence: true
