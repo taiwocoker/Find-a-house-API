e9c9beac7f6f2b8bbbd0d37bf11156954a0ff4bf
taiwocoker <taiwofcoker@gmail.com>
2020-11-13 09:29:17 +0100

    Add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
- include ActionController::Serialization
+  include ActionController::Serialization
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid
+  
+  private
+  def not_found(errors)
+    render json: errors, status: :not_found
+  end
+  def invalid(errors)
+    render json: errors, status: :unprocessable_entity
+  end
 end
