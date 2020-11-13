19af3a05677ef3d0d90142169aad0f31389615f6
taiwocoker <taiwofcoker@gmail.com>
2020-11-13 16:13:24 +0100

    Build method for new favourite houses

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,28 @@
 module Api::V1
   class UsersController < SecuredController
+    before_action :set_user, only: %i[create]
     def create
-      @user = User.find_by(email: params[:email])
       return render json: @user, status: :ok if @user
       user = User.create!(user_params)
       render json: user, status: :created
     end
+
+    def new_favourite
+      @user = User.find_by(id: favourite_params[:user_id])
+      @house = House.find_by(id: favourite_params[:house_id])
+      favourite = Favourite.create!(favourite_params)
+      render json: favourite, status: :created
+    end
+
     private
+    def set_user
+      @user = User.find_by(email: params[:email])
+    end
     def user_params
       params.permit(:username, :email)
     end
+    def favourite_params
+      params.permit(:house_id, :user_id)
+    end
   end
 end
-
