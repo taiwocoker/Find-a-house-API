763331fd150ee87d3242512c078195a2d414fb72
taiwocoker <taiwofcoker@gmail.com>
2020-11-13 09:20:49 +0100

    Migrate database to create favourite and house tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_111318) do
+ActiveRecord::Schema.define(version: 2020_11_13_081556) do
+
+  create_table "favourites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "house_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["house_id"], name: "index_favourites_on_house_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
+  create_table "houses", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,4 +35,6 @@ ActiveRecord::Schema.define(version: 2020_11_06_111318) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "favourites", "houses"
+  add_foreign_key "favourites", "users"
 end
