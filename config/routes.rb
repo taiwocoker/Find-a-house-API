b25140e58870a2240b8320d72a12045764f555dc
taiwocoker <taiwofcoker@gmail.com>
2020-11-14 09:52:11 +0100

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+      root 'houses#index'
   scope module: 'api', path: '/'  do
     namespace :v1, path: '/'  do
-      root 'houses#index'
       resources :users, only: [:create]
       resources :houses, only: [:index, :show, :create]
       post 'favourites', to: 'users#new_favourite'
