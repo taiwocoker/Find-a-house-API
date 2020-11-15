32ae9015bf492803005f59988656a39a339407db
taiwocoker <taiwofcoker@gmail.com>
2020-11-15 02:28:31 +0100

    Add functionality

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -4,10 +4,20 @@ module Api::V1
     def index
       render json: House.all
     end
+
+    def create
+      house = House.create!(house_params)
+      render json: house, status: :created
+    end
     
     def show
       @house = House.find(params[:id])
       return render json: @house, status: :ok if @house
+    end
+
+    private
+    def house_params
+      params.permit(:description, :name, :image)
     end
     
   end
