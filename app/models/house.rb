ebbf9e092b2a7e34f36382693419da20c5c58dbb
taiwocoker <taiwofcoker@gmail.com>
2020-11-15 18:38:12 +0100

    Refactor house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,11 +1,13 @@
 class House < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   has_many :favourites
   has_many :users, through: :favourites
   has_one_attached :image
   validates :name, :description, :image, presence: true
 
   def img_url
-    Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true)
+    url_for(image)
   end
 
 end
